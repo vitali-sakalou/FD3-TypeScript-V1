37de2fd21066402c760c85bcd4526a640b242a4d
loktevalexey <loktev.alex.74@gmail.com>
2017-11-21 20:22:51 +0300

    -120

---
+++ A_TS0-TS1/TS110_types_type/app.ts
@@ -0,0 +1,5 @@
+
+type StringList=Array<String>;
+
+var sl1:StringList=["a","b","c"];
+var sl2:StringList=sl1;
---
+++ A_TS0-TS1/TS120_types_convert/app.ts
@@ -0,0 +1,11 @@
+
+var n1:any=5;
+
+var s1:String=n1;
+console.log(s1);
+
+var s2:String=<String>n1;
+console.log(s2);
+
+var s3:String=n1 as String;
+console.log(s3);
