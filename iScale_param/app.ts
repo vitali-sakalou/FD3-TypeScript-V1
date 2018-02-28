3c985db89e65d330cc96b49b0091a46e9851a2a1
Vitali Sakalou <vitali.sakalou@gmail.com>
2018-03-01 00:10:50 +0300

    error

--- iScale_param/app.ts
+++ iScale_param/app.ts
@@ -1,104 +1,104 @@
-interface IStorageEngine{
+// interface IStorageEngine{
 
-     addItem(item):number;
-     getItem(index):number; 
-     getCount():number;
+//      addItem(item):number;
+//      getItem(index):number; 
+//      getCount():number;
 
-}
+// }
 
-class Scales<StorageEngine extends IStorageEngine> {
+// class Scales<StorageEngine extends IStorageEngine> {
 
-    addedProducts:Product[];
+//     addedProducts:Product[];
 
-    constructor() {
-        this.addedProducts=[]; 
-    }
+//     constructor() {
+//         this.addedProducts=[]; 
+//     }
   
-}
+// }
 
-class ScalesStorageEngineArray implements IStorageEngine {
+// // class ScalesStorageEngineArray implements IStorageEngine {
 
-     addItem(_addedProducts:Product):number {
-        this.addedProducts.push(_addedProducts);
-        console.log( this.addedProducts);
-        return (this.addedProducts.length)
-    };
-     getItem(index):number{}; 
-     getCount():number{};
+// //      addItem(_addedProducts:Product):number {
+// //         this.addedProducts.push(_addedProducts);
+// //         console.log( this.addedProducts);
+// //         return (this.addedProducts.length)
+// //     };
+// //      getItem(index):number{}; 
+// //      getCount():number{};
 
-}
+// // }
 
-class ScalesStorageEngineLocalStorage  implements IStorageEngine {
+// // class ScalesStorageEngineLocalStorage  implements IStorageEngine {
 
-    addItem(item):number{r};
-    getItem(index):number{}; 
-    getCount():number{};
+// //     addItem(item):number{r};
+// //     getItem(index):number{}; 
+// //     getCount():number{};
 
-}
+// // }
 
-let ScalesStorageEngineArrayScale=new Scales<ScalesStorageEngineArray>();
+// // let ScalesStorageEngineArrayScale=new Scales<ScalesStorageEngineArray>();
 
-let ScalesStorageEngineLocalStorageScale=new Scales<ScalesStorageEngineLocalStorage>();
+// // let ScalesStorageEngineLocalStorageScale=new Scales<ScalesStorageEngineLocalStorage>();
 
 
 
-class Product {
+// class Product {
 
-    name: string;
+//     name: string;
 
-    scale: number;
+//     scale: number;
 
-    getName():string{
-        return this.name;
-    }
+//     getName():string{
+//         return this.name;
+//     }
 
-    getScale():number{
-        return this.scale;
-    }
-    constructor(_getName, _getScale) {
-        this.name = _getName;
-        this.scale = _getScale;
-    }
-}
+//     getScale():number{
+//         return this.scale;
+//     }
+//     constructor(_getName, _getScale) {
+//         this.name = _getName;
+//         this.scale = _getScale;
+//     }
+// }
 
-class Apple extends Product {
+// class Apple extends Product {
 
-    type:string = 'apple';
+//     type:string = 'apple';
 
-    constructor(_getName, _getScale) {
-        super(_getName, _getScale);
-    }
+//     constructor(_getName, _getScale) {
+//         super(_getName, _getScale);
+//     }
 
-}
+// }
 
-class Tomato extends Product {
+// class Tomato extends Product {
 
-    type:string = 'tomato';
+//     type:string = 'tomato';
 
-    constructor(_getName, _getScale) {
-        super(_getName, _getScale);
-    }
+//     constructor(_getName, _getScale) {
+//         super(_getName, _getScale);
+//     }
     
-}
+// }
 
 
-let scale = new Scales();
+// let scale = new Scales();
 
 
-let GreenApple = new Apple("Зеленое яблоко", 300);
-let RedApple = new Apple("Красное яблоко", 350);
-let YellowApple = new Apple("Желтое яблоко", 400);
+// let GreenApple = new Apple("Зеленое яблоко", 300);
+// let RedApple = new Apple("Красное яблоко", 350);
+// let YellowApple = new Apple("Желтое яблоко", 400);
 
-let GreenTomato = new Tomato("Зеленый помидор", 200);
-let RedTomato = new Tomato("Красный помидор", 250);
-let YellowTomato = new Tomato("Желтый помидор", 280);
+// let GreenTomato = new Tomato("Зеленый помидор", 200);
+// let RedTomato = new Tomato("Красный помидор", 250);
+// let YellowTomato = new Tomato("Желтый помидор", 280);
 
-scale.add(GreenApple);
-scale.add(RedApple);
-scale.add(YellowApple);
-scale.add(GreenTomato);
-scale.add(RedTomato);
-scale.add(YellowTomato);
+// scale.add(GreenApple);
+// scale.add(RedApple);
+// scale.add(YellowApple);
+// scale.add(GreenTomato);
+// scale.add(RedTomato);
+// scale.add(YellowTomato);
 
-scale.getNameList();
-scale.getSumScale();
+// scale.getNameList();
+// scale.getSumScale();
